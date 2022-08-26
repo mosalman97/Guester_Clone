00db68e64c6637aae3ded32a517e5aa4ed20f4d0
salman@49 <salman@steyp.com>
2022-08-26 20:23:27 +0530

    constant file added and fontfamily and screensize added

---
+++ src/Constants.js
@@ -0,0 +1,14 @@
+import {
+  heightPercentageToDP as Hp,
+  widthPercentageToDP as Wp,
+} from 'react-native-responsive-screen';
+
+export const SIZE = {
+  Wp: Wp,
+  Hp: Hp,
+};
+export const MainFonts = {
+  dmBold: 'Bold',
+  dmMedium:'Medium',
+  dmRegular:'Regular'
+};
